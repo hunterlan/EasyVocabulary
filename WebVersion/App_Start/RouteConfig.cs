19322cd8d3aa161d04bfdb9d3052d616fb738de7
hunterlan <kotyshe@gmail.com>
2019-09-12 13:55:18 +0300

    Continue working with site

--- WebVersion/App_Start/RouteConfig.cs
+++ WebVersion/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WebVersion
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Start", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ WebVersion/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebVersion.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
