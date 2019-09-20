79f446bea0b9a698078227c78a8ed0bfdab8581a
hunterlan <kotyshe@gmail.com>
2019-09-21 00:09:52 +0300

    Fix a bit design

--- DesktopVersion/View/User/MainWindow.xaml.cs
+++ DesktopVersion/View/User/MainWindow.xaml.cs
@@ -19,7 +19,7 @@ namespace DesktopVersion
         public MainWindow()
         {
             InitializeComponent();
-            Internalization();
+            //Internalization();
         }
 
         private void Internalization()
